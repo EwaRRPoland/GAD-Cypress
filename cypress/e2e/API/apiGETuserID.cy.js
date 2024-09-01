4268a4aa0d8fde1c23ade44b56994653b4cd9aef
Ewa Rostecka-Rzońca <151127505+EwaRRPoland@users.noreply.github.com>
2024-09-01 16:19:44 +0200

    attempt to fix the test adding a reference to access_token

--- cypress/e2e/API/apiGETuserID.cy.js
+++ cypress/e2e/API/apiGETuserID.cy.js
@@ -1,14 +1,30 @@
+// describe('httpGAD tests GET user by ID', () => {
+//   it('response code should be 200 and body should not be empty', () => {
+//     cy.request('http://localhost:3000/api/users/30').then(response => {
+//       const status = response.status;
+//       const body = response.body;
+
+//       assert.equal(200, status);
+//         assert.isNotEmpty(body, 'Response body is not empty');
+//         cy.log('Response body:', response.body);
+//     });
+//   });
+// });
 describe('httpGAD tests GET user by ID', () => {
   it('response code should be 200 and body should not be empty', () => {
-    cy.request('http://localhost:3000/api/users/30').then(response => {
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:3000/api/users/30',
+      headers: {
+        'Authorization': 'Bearer your_access_token_here'
+      }
+    }).then(response => {
       const status = response.status;
       const body = response.body;
 
       assert.equal(200, status);
-        assert.isNotEmpty(body, 'Response body is not empty');
-      //   	Response body - console
-        //console.log(body);
-        cy.log('Response body:', response.body);
+      assert.isNotEmpty(body, 'Response body is not empty');
+      cy.log('Response body:', response.body);
     });
   });
 });
