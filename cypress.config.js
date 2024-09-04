16cd9d0ac0a236108b11a41103ccad5279556a2b
Ewa Rostecka-Rzońca <151127505+EwaRRPoland@users.noreply.github.com>
2024-09-04 17:29:55 +0200

    changes in main.yml 4

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'results/cypress-report',
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
-  },
+    baseUrl: 'http://localhost:3000'
+  }
 });
